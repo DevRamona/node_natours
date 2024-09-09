b657d3092051f3b55e15ff04163e3fcf075f9dc7
DevRamona <r.ingabire@alustudent.com>
2024-09-10 00:16:45 +0200

    how to do a post request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { json } = express;
 const fs = require("fs");
 const app = express();
+app.use(express.json())
 // app.get("/", (request, response) => {
 //     response.status(200).json({message:"Hello from the server side", app:"naptours"})
 // })
@@ -13,7 +14,7 @@ const app = express();
 let tours;
 try {
     tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)) 
-    console.log(`${__dirname}/dev-data/data/tours-simple.json`);
+    // console.log(`${__dirname}/dev-data/data/tours-simple.json`);
 }catch(error) {
     console.log("Error faced",error.message )
 }
@@ -26,6 +27,23 @@ app.get('/api/v1/tours', (request,response) => {
         }
 
     })
+})
+app.post('/api/v1/tours', (request, response) => {
+    const newId = tours[tours.length -1].id + 1
+    const newTour = Object.assign({id:newId}, request.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        response.status(201).json({
+            status: "success",
+            results: tours.length,
+            data: {
+                tours:newTour
+            }
+        
+    })
+    
+    
+})
 })
 const port = 3000
 app.listen(port, () =>{
